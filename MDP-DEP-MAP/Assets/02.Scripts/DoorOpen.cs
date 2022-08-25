0418e523e76798c205a4bff5f8390801bbf6d5e5
123123445 <102730775+123123445@users.noreply.github.com>
2022-08-25 20:35:19 +0900

    2022_08_25_08:35

--- MDP-DEP-MAP/Assets/02.Scripts/DoorOpen.cs
+++ MDP-DEP-MAP/Assets/02.Scripts/DoorOpen.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class DoorOpen : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float last_state = 1;
+    public bool is_Open_Door=true;
+    
+    public void Open()
     {
-        
+        if (is_Open_Door == true)
+        {
+            StartCoroutine("DoorOff");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DoorOff()
     {
-        
+            is_Open_Door = false;
+            for (int i = 0; i < 90; i++)
+            {
+                transform.Rotate(new Vector3(0, 1 * last_state, 0));
+                yield return null;
+            }
+        last_state *= -1;
+        is_Open_Door = true;
     }
 }
